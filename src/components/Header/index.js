a68c1c6f492b121e376cf3ba59011ea741a2cf99
Kerosz <andrei_chirila@outlook.com>
2020-11-12 17:12:18 +0200

    testing git

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,8 +2,7 @@ import React, {Component} from 'react';
 
 class Header extends Component {
     render(){
-
-        return <h1>This is the Header</h1>
+        return <h1>This is the a Nice Header</h1>
     }
 }
 
