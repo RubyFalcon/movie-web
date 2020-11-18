6dcaf5069a4cef514042a50aef15fe6f14a9751a
Kerosz <andrei_chirila@outlook.com>
2020-11-18 20:50:25 +0200

    fixed env vars

--- config/.dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    PORT: process.env.PORT || 5000,
-    DB_HOST: "127.0.0.1",
-    DB_USER: "your_database_user",
-    DB_PASSWORD: "your_database_password",
-    DB_NAME: "myapp_test"
-}
--- server.js
+++ server.js
@@ -1,31 +1,45 @@
 const express = require("express");
 const knex = require('knex');
+const chalk = require('chalk');
 const api = require("./api");
-const config = require("./config/key")
 
+//  Loads environment variables from .env file
+if (process.env.NODE_ENV !== 'production') {
+    require("dotenv").config({
+        path: `.env.development`
+    })
+}
+
+//  Creates express server
 const app = express();
 
-//config db
+// DB configuration
 knex({
     client: 'pg',
     version: '13.1',
     connection: {
-        host : config.DB_HOST,
-        user : config.DB_USER,
-        password : config.DB_PASSWORD,
-        database : config.DB_NAME
+        host : process.env.PGHOST,
+        port : process.env.PGPORT,
+        user : process.env.PGUSER,
+        password : process.env.PGPASSWORD,
+        database : process.env.PGDATABASEq
     },
     acquireConnectionTimeout: 10000
 });
 
-//config express
+//  Express configuration
+app.set("host", process.env.APP_ADDRESS || "0.0.0.0");
+app.set("port", process.env.APP_PORT || 5000);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
-// config api
+//  API routes
 app.use("api/v1", api);
-
 app.get("/", (req, res) => res.send("Hello friend!"));
 
+//  Starts exoress
+app.listen(app.get('port'), () => {
+    console.log('%s App is running at %s:%d in %s mode\n  Press CTRL-C to stop\n', chalk.green('✓'), app.get("host"), app.get('port'), app.get('env'));
+});
 
-app.listen(config.PORT, () => console.log(`App listens on port ${config.PORT}`));
+module.exports = app;
