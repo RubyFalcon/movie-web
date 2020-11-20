34083024b7d19a1548d9e52615cf4ace44313f8d
Kerosz <andrei_chirila@outlook.com>
2020-11-20 17:44:42 +0200

    added cors/helmet && hooked up react to express

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const knex = require('knex');
+const cors = require("cors");
+const helmet = require("helmet");
+const logger = require("morgan");
+const path = require("path");
 const chalk = require('chalk');
 const dotenv = require("dotenv");
 const api = require("./api");
@@ -15,8 +19,6 @@ if (process.env.NODE_ENV !== 'production') {
         path: "config/.env.production"
     });
 };
-
-
 
 //  Creates express server
 const app = express();
@@ -38,11 +40,17 @@ knex({
 //  Express configuration
 app.set("host", process.env.APP_ADDRESS || "0.0.0.0");
 app.set("port", process.env.APP_PORT || 5000);
-app.use(express.json());
+app.use(cors());
+app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(logger("dev"));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 //  API routes
-app.get("/", (req, res) => res.send("Hello friend!"));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 app.use('/api/v1', api);
 
 //  Error handling
@@ -53,10 +61,16 @@ app.all("*", (req, res, next) => {
 });
 app.use(errorHandler);
 
-//  Starts exoress
-app.listen(app.get('port'), () => {
-    console.log('%s App is running at %s:%d in %s mode', chalk.green('✓'), app.get("host"), app.get('port'), app.get('env'));
-    console.log("  Press CTRL-C to stop\n");
+//  Starts express
+app.listen(app.get('port'), (err) => {
+
+    if (err) {
+        console.error(err);
+    } else {
+        console.log('%s App is running at %s:%d in %s mode', chalk.green('✓'), app.get("host"), app.get('port'), app.get('env'));
+        console.log("  Press CTRL-C to stop\n");
+    }
+
 });
 
 module.exports = app;
